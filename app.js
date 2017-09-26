0a8bf510ad3de12aa258471ab754282015eb6788
Cuwor <temichit@yandex.ru>
2017-09-26 15:24:48 +0700

    delete tags

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 var vk = require('./vkApiService.js')();
-var username = '89082045697';// С ПРИВЯЗКОЙ К ТЕЛЕФОНУ!!! Иначе не работает
+var username = '89082045697'; // С ПРИВЯЗКОЙ К ТЕЛЕФОНУ!!! Иначе не работает
 var password = 'dl494kr9ir4ori';
 
 
@@ -9,14 +9,21 @@ vk.login(username, password)
   .then(res => {
       var access_token;
       access_token = res.access_token;
-      console.log(access_token);
+      //console.log(access_token);
       vk.stealPost(-151476691, access_token).then(res => {
-        console.log(res.response.wall);
+        //console.log(res.response.wall);
         var text = res.response.wall[2].text;
-        //var text = '<br><br><br><br><br> потом мы ищем всякие #теги вот так';
-        text = text.replace(/<br>/gi,"\n");
-        // var tag = text.match(/#/ig)
-        // text = text.replace(tag[0], '');
+        var textList = text.split(' ');
+        for (var i = 0; i < textList.length; i++) {
+          var tag = textList[i].match(/#/ig);
+          if (tag != null) {
+            textList.splice(i,1);
+          }
+        }
+        //console.log(textList);
+        text = textList.join(' ');
+        text = text.replace(/<br>/gi, "\n");
+        console.log(text);
         var attachments = 'photo' + res.response.wall[2].media.owner_id + '_' + res.response.wall[2].media.item_id;
         vk.putPost(-153992110, text, attachments, access_token).then(res => {
           console.log(text);
