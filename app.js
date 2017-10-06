d75ccdf4f186477fa3b66998e9eb64fc2203a27e
Cuwor <temichit@yandex.ru>
2017-10-06 18:01:38 +0700

    Интервалы работы и поста

--- app.js
+++ app.js
@@ -16,72 +16,102 @@ app.use(bodyParser.json())
 var date = new Date();
 var current_hour = date.getHours();
 var current_min = date.getMinutes();
+var current_time = current_hour * 3600 + current_min * 60;
 
-vk.login(config.username, config.password)
-  .then(res => {
-        steal(res);
-    },
-    error => {
-      console.log("Rejected: " + error);
-      if (error.error.error == "need_captcha") {
-        console.log(error.error.captcha_sid);
-        captcha(error);
-      }
-    })
 
+
+setTimeout(login(), 100);
+setInterval(login(), config.post_interval_finish_inSec*1000)
+
+
+
+function login() {
+  return function() {
+    vk.login(config.username, config.password)
+      .then(res => {
+          config.work_interval.forEach(function(item) {
+            var interval = item.replace('-', ':');
+            var inter = interval.split(':');
+            var timeStart = 3600 * inter[0] + inter[1] * 60;
+            var timeFinish = 3600 * inter[2] + inter[3] * 60;
+            var posted = false;
+
+            if (current_time >= timeStart && current_time <= timeFinish) {
+              steal(res);
+            }else {
+              console.log("Не время для постов");
+            }
+          })
+
+        },
+        error => {
+          console.log("Rejected: " + error);
+          if (error.error.error == "need_captcha") {
+            console.log(error.error.captcha_sid);
+            captcha(error);
+          }
+        })
+  }
+}
 
 function steal(res) {
-  while (current_hour >= config.startHour && current_hour <= config.finishHour && current_min >= config.startMinutes && current_min <= config.finishMinutes) {
-    var access_token;
-    access_token = res.access_token;
-    console.log(access_token);
-    vk.stealPost(config.group_steal, access_token).then(resp => {
-      if (resp.response.wall[1].is_pinned != 1) {
-        var post_id = resp.response.wall[1].id;
-      } else {
-        var post_id = resp.response.wall[2].id;
-      }
-      vk.stealDataPost(config.group_steal, post_id, access_token).then(res => {
-        //console.log(res.response);
-        var text = res.response[0].text;
-        var urlSearch = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/
-        if (text.search(urlSearch) == -1) {
+  var access_token;
+  access_token = res.access_token;
+  //console.log(access_token);
 
-          var textList = text.split(' ');
-          for (var j = 0; j < textList.length; j++) {
-            var tag = textList[j].match(/#/ig);
-            if (tag != null) {
-              textList.splice(j, 1);
-            }
+  vk.stealPost(config.group_steal, access_token).then(resp => {
+    if (resp.response.wall[1].is_pinned != 1) {
+      var post_id = resp.response.wall[1].id;
+    } else {
+      var post_id = resp.response.wall[2].id;
+    }
+
+    vk.stealDataPost(config.group_steal, post_id, access_token).then(res => {
+      //console.log(res.response);
+      var text = res.response[0].text;
+      var urlSearch = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/
+      if (text.search(urlSearch) == -1) {
+
+        var textList = text.split(' ');
+        for (var j = 0; j < textList.length; j++) {
+          var tag = textList[j].match(/#/ig);
+          if (tag != null) {
+            textList.splice(j, 1);
           }
-
-          //console.log(textList);
-          text = textList.join(' ');
-          text = text.replace(/<br>/gi, "\n");
-          //console.log(text);
-
-          var attachList = res.response[0].attachments;
-          var attachments = "";
-          for (var i = 0; i < attachList.length; i++) {
-            if (attachList[i].type == 'doc') {
-              attachments += attachList[i].type + attachList[i].doc.owner_id + '_' + attachList[i].doc.did + ",";
-            } else if (attachList[i].type == 'photo') {
-              attachments += attachList[i].type + attachList[i].photo.owner_id + '_' + attachList[i].photo.pid + ",";
-            } else if (attachList[i].type == 'audio') {
-              attachments += attachList[i].type + attachList[i].audio.owner_id + '_' + attachList[i].audio.aid + ",";
-            }
-          }
-
-          vk.putPost(config.group_put, text, attachments, access_token).then(res => {
-              console.log(text);
-            })
-            .catch(onRejected => {
-              console.log('Ошииииииииибка -------------------------- ' + onRejected);
-            })
         }
+        text = textList.join(' ');
+        text = text.replace(/<br>/gi, "\n");
 
-      });
+        var attachList = res.response[0].attachments;
+        var attachments = "";
+        for (var i = 0; i < attachList.length; i++) {
+          if (attachList[i].type == 'doc') {
+            attachments += attachList[i].type + attachList[i].doc.owner_id + '_' + attachList[i].doc.did + ",";
+          } else if (attachList[i].type == 'photo') {
+            attachments += attachList[i].type + attachList[i].photo.owner_id + '_' + attachList[i].photo.pid + ",";
+          } else if (attachList[i].type == 'audio') {
+            attachments += attachList[i].type + attachList[i].audio.owner_id + '_' + attachList[i].audio.aid + ",";
+          }
+        }
+        var randomInterval = Math.random() * (config.post_interval_finish_inSec - config.post_interval_start_inSec) + config.post_interval_start_inSec;
+        randomInterval = randomInterval * 1000;
+        console.log(randomInterval);
+        setTimeout(putPost(text, attachments, access_token), randomInterval);
+      }
     });
+  });
+
+
+}
+
+function putPost(text, attachments, access_token) {
+  return function() {
+    vk.putPost(config.group_put, text, attachments, access_token).then(res => {
+        console.log(text);
+      })
+      .catch(onRejected => {
+        console.log('Ошииииииииибка -------------------------- ' + onRejected);
+      })
   }
 }
 
