1b3a0fb921df0fe8411201e1e7047cddb87f42dd
Cuwor <temichit@yandex.ru>
2017-10-28 15:49:56 +0700

    fix null exeption

--- programm/app.js
+++ programm/app.js
@@ -16,12 +16,12 @@ var date = new Date();
 var current_hour = date.getHours();
 var current_min = date.getMinutes();
 var current_time = current_hour * 3600 + current_min * 60;
-var last_post_id = 5;
+var last_post_id = 0;
 
 
 
 setTimeout(login(), 100);
-setInterval(login(), config.post_interval_finish_inSec*1000)
+setInterval(login(), config.post_interval_finish_inSec * 1000)
 
 
 
@@ -38,7 +38,7 @@ function login() {
 
             if (current_time >= timeStart && current_time <= timeFinish) {
               steal(res);
-            }else {
+            } else {
               console.log("Не время для постов");
             }
           })
@@ -60,48 +60,56 @@ function steal(res) {
   //console.log(access_token);
 
   vk.stealPost(config.group_steal, access_token).then(resp => {
-    if (resp.response.wall[1].is_pinned != 1) {
-      var post_id = resp.response.wall[1].id;
-    } else {
-      var post_id = resp.response.wall[2].id;
-    }
+    if (resp.response.wall[1] != null) {
 
-    vk.stealDataPost(config.group_steal, post_id, access_token).then(res => {
-      //console.log(res.response);
-      var text = res.response[0].text;
-      var urlSearch = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/
-      if (text.search(urlSearch) == -1 && last_post_id != post_id) {
-        last_post_id = post_id;
-        console.log(last_post_id);
-        var textList = text.split(' ');
-        for (var j = 0; j < textList.length; j++) {
-          var tag = textList[j].match(/#/ig);
-          if (tag != null) {
-            textList.splice(j, 1);
-          }
-        }
-        text = textList.join(' ');
-        text = text.replace(/<br>/gi, "\n");
 
-        var attachList = res.response[0].attachments;
-        var attachments = "";
-        for (var i = 0; i < attachList.length; i++) {
-          if (attachList[i].type == 'doc') {
-            attachments += attachList[i].type + attachList[i].doc.owner_id + '_' + attachList[i].doc.did + ",";
-          } else if (attachList[i].type == 'photo') {
-            attachments += attachList[i].type + attachList[i].photo.owner_id + '_' + attachList[i].photo.pid + ",";
-          } else if (attachList[i].type == 'audio') {
-            attachments += attachList[i].type + attachList[i].audio.owner_id + '_' + attachList[i].audio.aid + ",";
-          }
-        }
-        var randomInterval = Math.random() * (config.post_interval_finish_inSec - config.post_interval_start_inSec) + config.post_interval_start_inSec;
-        randomInterval = randomInterval * 1000;
-        console.log(randomInterval);
-        setTimeout(putPost(text, attachments, access_token), randomInterval);
+      if (resp.response.wall[1].is_pinned != 1) {
+        var post_id = resp.response.wall[1].id;
       } else {
-        console.log('Данный id уже использован или содержит url');
+        var post_id = resp.response.wall[2].id;
       }
-    });
+
+      vk.stealDataPost(config.group_steal, post_id, access_token).then(res => {
+        //console.log(res.response);
+        var text = res.response[0].text;
+        var urlSearch = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/
+        if (text.search(urlSearch) == -1 && last_post_id != post_id) {
+          last_post_id = post_id;
+          console.log(last_post_id);
+          var textList = text.split(' ');
+          for (var j = 0; j < textList.length; j++) {
+            var tag = textList[j].match(/#/ig);
+            if (tag != null) {
+              textList.splice(j, 1);
+            }
+          }
+          text = textList.join(' ');
+          text = text.replace(/<br>/gi, "\n");
+          if (res.response[0].attachments != null) {
+
+            var attachList = res.response[0].attachments;
+            var attachments = "";
+            for (var i = 0; i < attachList.length; i++) {
+              if (attachList[i].type == 'doc') {
+                attachments += attachList[i].type + attachList[i].doc.owner_id + '_' + attachList[i].doc.did + ",";
+              } else if (attachList[i].type == 'photo') {
+                attachments += attachList[i].type + attachList[i].photo.owner_id + '_' + attachList[i].photo.pid + ",";
+              } else if (attachList[i].type == 'audio') {
+                attachments += attachList[i].type + attachList[i].audio.owner_id + '_' + attachList[i].audio.aid + ",";
+              }
+            }
+          }
+          var randomInterval = Math.random() * (config.post_interval_finish_inSec - config.post_interval_start_inSec) + config.post_interval_start_inSec;
+          randomInterval = randomInterval * 1000;
+          console.log(randomInterval);
+          setTimeout(putPost(text, attachments, access_token), randomInterval);
+        } else {
+          console.log('Данный id уже использован или содержит url');
+        }
+      });
+    } else {
+      console.log('Постов нет');
+    }
   });
 
 
